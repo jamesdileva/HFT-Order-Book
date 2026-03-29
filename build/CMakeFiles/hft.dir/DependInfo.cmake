afc44dfcd89fcc2145a7207dbbb7cf4e1ef5175b
jamesdileva <jamesdileva@gmail.com>
2026-03-29 02:14:50 -0700

    Added StockProfile system: 6 stock personalities with regime and event engine

--- build/CMakeFiles/hft.dir/DependInfo.cmake
+++ build/CMakeFiles/hft.dir/DependInfo.cmake
@@ -9,10 +9,17 @@ set(CMAKE_DEPENDS_LANGUAGES
 # The set of dependency files which are needed:
 set(CMAKE_DEPENDS_DEPENDENCY_FILES
   "C:/Users/j/HFT-Order-Book/src/Benchmark.cpp" "CMakeFiles/hft.dir/src/Benchmark.cpp.obj" "gcc" "CMakeFiles/hft.dir/src/Benchmark.cpp.obj.d"
+  "C:/Users/j/HFT-Order-Book/src/GUI.cpp" "CMakeFiles/hft.dir/src/GUI.cpp.obj" "gcc" "CMakeFiles/hft.dir/src/GUI.cpp.obj.d"
   "C:/Users/j/HFT-Order-Book/src/LatencyProfiler.cpp" "CMakeFiles/hft.dir/src/LatencyProfiler.cpp.obj" "gcc" "CMakeFiles/hft.dir/src/LatencyProfiler.cpp.obj.d"
   "C:/Users/j/HFT-Order-Book/src/MatchingEngine.cpp" "CMakeFiles/hft.dir/src/MatchingEngine.cpp.obj" "gcc" "CMakeFiles/hft.dir/src/MatchingEngine.cpp.obj.d"
   "C:/Users/j/HFT-Order-Book/src/OrderBook.cpp" "CMakeFiles/hft.dir/src/OrderBook.cpp.obj" "gcc" "CMakeFiles/hft.dir/src/OrderBook.cpp.obj.d"
   "C:/Users/j/HFT-Order-Book/src/OrderGenerator.cpp" "CMakeFiles/hft.dir/src/OrderGenerator.cpp.obj" "gcc" "CMakeFiles/hft.dir/src/OrderGenerator.cpp.obj.d"
+  "C:/Users/j/HFT-Order-Book/src/imgui.cpp" "CMakeFiles/hft.dir/src/imgui.cpp.obj" "gcc" "CMakeFiles/hft.dir/src/imgui.cpp.obj.d"
+  "C:/Users/j/HFT-Order-Book/src/imgui_draw.cpp" "CMakeFiles/hft.dir/src/imgui_draw.cpp.obj" "gcc" "CMakeFiles/hft.dir/src/imgui_draw.cpp.obj.d"
+  "C:/Users/j/HFT-Order-Book/src/imgui_impl_opengl3.cpp" "CMakeFiles/hft.dir/src/imgui_impl_opengl3.cpp.obj" "gcc" "CMakeFiles/hft.dir/src/imgui_impl_opengl3.cpp.obj.d"
+  "C:/Users/j/HFT-Order-Book/src/imgui_impl_sdl2.cpp" "CMakeFiles/hft.dir/src/imgui_impl_sdl2.cpp.obj" "gcc" "CMakeFiles/hft.dir/src/imgui_impl_sdl2.cpp.obj.d"
+  "C:/Users/j/HFT-Order-Book/src/imgui_tables.cpp" "CMakeFiles/hft.dir/src/imgui_tables.cpp.obj" "gcc" "CMakeFiles/hft.dir/src/imgui_tables.cpp.obj.d"
+  "C:/Users/j/HFT-Order-Book/src/imgui_widgets.cpp" "CMakeFiles/hft.dir/src/imgui_widgets.cpp.obj" "gcc" "CMakeFiles/hft.dir/src/imgui_widgets.cpp.obj.d"
   "C:/Users/j/HFT-Order-Book/src/main.cpp" "CMakeFiles/hft.dir/src/main.cpp.obj" "gcc" "CMakeFiles/hft.dir/src/main.cpp.obj.d"
   )
 
